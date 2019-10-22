ec2399f2fd2e8f4a18c21c7e3c7d5d198e9f6ade
lekaren <cpdnql@naver.com>
2019-10-22 14:11:18 +0900

    Hooks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import React, { Component } from "react";
-import LifeCycleSample from "./7/LifeCycleSample";
-import ErrorBoundary from "./7/ErrorBoundary";
+import React from "react";
+import Average from "./8/Average";
 
-// 랜덤 색상을 생성합니다.
-function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
-class App extends Component {
-  state = {
-    color: "#000000"
-  };
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor()
-    });
-  };
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤 색상</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <Average />;
+};
+
 export default App;
